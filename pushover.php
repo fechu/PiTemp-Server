839acf7a0e835b2b6747ed993eca0ff42d06921f
Sandro Meier <sandro.meier@fidelisfactory.ch>
2013-08-02 12:04:00 +0200

    Initial commit

---
+++ pushover.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * This file receives 2 GET parameters: "temp" and "userkey"
+ * If both of them are valid a message is send to pushover. And pushover
+ * will forward it to your devices that have the pushover application installed.
+ * 
+ * @file pushover.php
+ * @author Sandro Meier
+ * @date 02.08.2013
+ */
+
+
+
+?>
