b896053290049382ea5522c2f8c622558b8dad5d
Sandro Meier <sandro.meier@fidelisfactory.ch>
2013-08-02 12:53:11 +0200

    Add configuration

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php 
+
+return array(
+
+	/**
+	 * Enter the token for your application on pushover. You shouldn't comit this
+	 * token in git. Keep it private.
+	 */
+	'pushover_token' => 'YOUR_PUSHOVER_APPLICATION_TOKEN'
+);
